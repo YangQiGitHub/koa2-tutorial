c29d37fdf1cf0bcbf7af14051b3825797c9577c3
yangqi02 <yangqi02@zuoyebang.com>
2019-12-01 16:27:22 +0800

    koa-router使用

--- app.js
+++ app.js
@@ -28,25 +28,5 @@ app
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods());
-// app.use(async (ctx) => {
-//   if (ctx.url === '/' && ctx.method === 'GET') {
-//     ctx.type = 'html';
-//     let html = `
-//       <h1>登录</h1>
-//       <form method="POST" action="/">
-//         <p>用户名</p>
-//         <input name="userName" /><br/>
-//         <p>密码</p>
-//         <input name="password" type="password" /><br/>
-//         <button type="submit">submit</button>
-//       </form>
-//     `;
-//     ctx.body = html;
-//   }
-//   if (ctx.url === '/' && ctx.method === 'POST') {
-//     let postData = ctx.request.body;
-//     ctx.body = postData;
-//   }
-// });
 
 app.listen(3000);
