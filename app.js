66804437a83520ab2b4928756e19f4d2258e6c76
yangqi02 <yangqi02@zuoyebang.com>
2019-12-01 15:29:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const koa = require('koa');
+const app = new koa();
+const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.type = 'html';
+  let html = `
+    <h1>登录</h1>
+    <form method="POST" action="/">
+      <p>用户名</p>
+      <input name="userName" /><br/>
+      <p>密码</p>
+      <input name="password" type="password" /><br/>
+      <button type="submit">submit</button>
+    </form>
+  `;
+  ctx.body = html;
+});
+
+router.post('/', (ctx, next) => {
+  let postData = ctx.request.body;
+  ctx.body = postData;
+});
+
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
+// app.use(async (ctx) => {
+//   if (ctx.url === '/' && ctx.method === 'GET') {
+//     ctx.type = 'html';
+//     let html = `
+//       <h1>登录</h1>
+//       <form method="POST" action="/">
+//         <p>用户名</p>
+//         <input name="userName" /><br/>
+//         <p>密码</p>
+//         <input name="password" type="password" /><br/>
+//         <button type="submit">submit</button>
+//       </form>
+//     `;
+//     ctx.body = html;
+//   }
+//   if (ctx.url === '/' && ctx.method === 'POST') {
+//     let postData = ctx.request.body;
+//     ctx.body = postData;
+//   }
+// });
+
+app.listen(3000);
